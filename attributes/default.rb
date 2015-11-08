036ac2c9bf5ad7bc4728469e99a0f086c1b88276
Joe Goggins <joe.goggins@gmail.com>
2015-11-08 15:33:29 -0600

    openocd version check update

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,9 @@ default['particle']['programmer_shield']['official_binaries'] = "#{node['particl
 default['particle']['programmer_shield']['openocd_packages'] = %w(build-essential make libtool pkg-config autoconf automake texinfo libusb-dev libusb-1.0-0 libusb-1.0-0-dev gdb)
 default['particle']['programmer_shield']['openocd_git_clone_url'] = 'git://git.code.sf.net/p/openocd/code'
 default['particle']['programmer_shield']['openocd_git_branch'] = 'v0.9.0'
-default['particle']['programmer_shield']['openocd_version_check'] = "openocd --version 2>&1 | grep #{node['particle']['programmer_shield']['openocd_git_branch']}"
+# This will be exit-code 0 when we're installed and good, and non 0 otherwise
+# This won't work unless the openocd_git_branch begins with a vX.Y.Z
+default['particle']['programmer_shield']['openocd_version_check'] = "which openocd && openocd --version 2>&1 | grep -F '#{node['particle']['programmer_shield']['openocd_git_branch'].gsub(/^v/,'')}'"
 default['particle']['programmer_shield']['openocd_git_clone_path'] = "#{node['particle']['programmer_shield']['shared_dir']}/openocd"
 
 # Environment variables + remote content + config
