8c4b81c8b17fc591b05a77478e5a6a5f27d78db1
Joe Goggins <joe.goggins@gmail.com>
2016-04-09 00:08:42 -0500

    drop screen

--- attributes/packages.rb
+++ attributes/packages.rb
@@ -6,7 +6,7 @@ default['particle-base']['do_update_packages'] = true
 
 # COMMON PACKAGES
 default['particle-base']['packages']['essentials'] = %w(git)
-default['particle-base']['packages']['terminal-multiplexers'] = %w(tmux screen)
+default['particle-base']['packages']['terminal-multiplexers'] = %w(tmux)
 default['particle-base']['packages']['dfu-util'] = %w(pkg-config libusb dfu-util)
 # From http://czmq.zeromq.org/page:get-the-software, "you will need build-essentials, libtool, autoconf, automake and uuid-dev or equivalent."
 # todo: look into
