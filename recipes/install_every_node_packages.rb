a0481f2ca83c8a1848e623c14b8556e9a20d66f1
Joe Goggins <joe.goggins@gmail.com>
2016-09-22 20:46:31 -0500

    Make the every node package installs be separate transactions to avoid overly constrained package dependencies

--- recipes/install_every_node_packages.rb
+++ recipes/install_every_node_packages.rb
@@ -1,10 +1,16 @@
 case node['platform_family']
 when 'debian' # includes ubuntu
-  package node['particle_base']['packages']['debian']['every_node']
+  node['particle_base']['packages']['debian']['every_node'].each do |p|
+    package p
+  end
 when 'mac_os_x'
-  package node['particle_base']['packages']['mac_os_x']['every_node']
+  node['particle_base']['packages']['mac_os_x']['every_node'].each do |p|
+    package p
+  end
 when 'rhel'
-  package node['particle_base']['packages']['rhel']['every_node']
+  node['particle_base']['packages']['rhel']['every_node'].each do |p|
+    package p
+  end
 else
   log "Cannot update package index for platform_family=#{node['platform_family']}" do
     level :error
