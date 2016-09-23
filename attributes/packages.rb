3578a24b316520057fe906a50f3e8a3bfa16fce1
Joe Goggins <joe.goggins@gmail.com>
2016-09-22 20:34:24 -0500

    Adds minimal base package list for rhel systems

--- attributes/packages.rb
+++ attributes/packages.rb
@@ -1,13 +1,16 @@
 # packages that should live EVERY WHERE; no packages that launch services allowed in this list; that should happen elsewhere
 default['particle_base']['packages']['debian']['every_node'] = %w(ca-certificates git htop tmux vim nano netcat jq)
 default['particle_base']['packages']['mac_os_x']['every_node'] = %w(git vim tmux nano netcat jq)
+default['particle_base']['packages']['rhel']['every_node'] = node['particle_base']['packages']['debian']['every_node']
 
-
+# package node['particle_base']['packages']['rhel']['every_node']
 # dfu packages for different OSes
 default['particle_base']['packages']['debian']['dfu'] = %w(pkg-config libusb-1.0-0 dfu-util)
 default['particle_base']['packages']['mac_os_x']['dfu'] = %w(pkg-config libusb dfu-util)
+# rhel not supported here
 
 # zmq packages
 # See http://czmq.zeromq.org/page:get-the-software for the latest instructions
 default['particle_base']['packages']['debian']['zmq'] = %w(software-properties-common)
 default['particle_base']['packages']['mac_os_x']['zmq'] = %w(libtool)
+# rhel not supported here
--- recipes/install_every_node_packages.rb
+++ recipes/install_every_node_packages.rb
@@ -3,6 +3,8 @@ when 'debian' # includes ubuntu
   package node['particle_base']['packages']['debian']['every_node']
 when 'mac_os_x'
   package node['particle_base']['packages']['mac_os_x']['every_node']
+when 'rhel'
+  package node['particle_base']['packages']['rhel']['every_node']
 else
   log "Cannot update package index for platform_family=#{node['platform_family']}" do
     level :error
