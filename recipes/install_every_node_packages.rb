5da4edae14022c9f7b986c41dab151d3a6c797ae
Joe Goggins <joe.goggins@gmail.com>
2016-09-22 20:50:25 -0500

    Revert "Make the every node package installs be separate transactions to avoid overly constrained package dependencies" This reverts commit a0481f2ca83c8a1848e623c14b8556e9a20d66f1, twas mistaken: from https://docs.chef.io/resource_package.html
    Specifing multiple packages and/or versions allows a single transaction
    to:
    
    - Download the specified packages and versions via a single HTTP
    transaction
    - Update or install multiple packages with a single resource during the
    chef-client run

--- recipes/install_every_node_packages.rb
+++ recipes/install_every_node_packages.rb
@@ -1,16 +1,10 @@
 case node['platform_family']
 when 'debian' # includes ubuntu
-  node['particle_base']['packages']['debian']['every_node'].each do |p|
-    package p
-  end
+  package node['particle_base']['packages']['debian']['every_node']
 when 'mac_os_x'
-  node['particle_base']['packages']['mac_os_x']['every_node'].each do |p|
-    package p
-  end
+  package node['particle_base']['packages']['mac_os_x']['every_node']
 when 'rhel'
-  node['particle_base']['packages']['rhel']['every_node'].each do |p|
-    package p
-  end
+  package node['particle_base']['packages']['rhel']['every_node']
 else
   log "Cannot update package index for platform_family=#{node['platform_family']}" do
     level :error
