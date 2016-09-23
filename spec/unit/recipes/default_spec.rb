27cd8a91432eb987ed8a0efbd8f114ad449add9a
Joe Goggins <joe.goggins@gmail.com>
2016-09-22 22:23:23 -0500

    Fix broken unit chefspec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'particle_base::default' do
+  before do
+    stub_command("which bats").and_return('/usr/local/bin/bats')
+  end
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) { ChefSpec::ServerRunner.converge(described_recipe) }
     it 'converges successfully' do
