1e56e64795947e55ef9cd274b7da4d52bd246300
Joe Goggins <joe.goggins@gmail.com>
2016-09-29 20:26:16 -0500

    Adds non default recipe to chef_gem install particle-spec gem

---
+++ attributes/particle_spec.rb
@@ -0,0 +1,2 @@
+# Pin it to a particular version here
+default['particle_base']['particle_spec']['version'] = "0.2.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,7 @@ include_recipe('particle_base::install_arm_gcc')
 include_recipe('particle_base::install_bats')
 include_recipe('particle_base::install_scripts')
 include_recipe('particle_base::install_particle_cli_ng')
+
+# Deliberately not included/increases the dependencies significantly
+# and doesn't work reliably on any platforms yet
+# include_recipe('particle_base::install_particle_spec_rubygem')
